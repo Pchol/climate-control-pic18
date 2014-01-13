c3e0d72b1c66171d63d1500e782173a339df01d7
Pchol <spchols@gmail.com>
2014-01-13 15:20:37 +0400

    проверка записи

--- main.c
+++ main.c
@@ -11,21 +11,23 @@
 void main(void) {
 
     unsigned char config1 = 0, config2 = 0, config3 = 0;
-    unsigned int adccount = 0;
+    unsigned int adccount[127], i=0;
 
 	config1 = ADC_FOSC_RC | ADC_RIGHT_JUST | ADC_2_TAD ;
 	config2 = ADC_CH2 | ADC_INT_OFF;
-	config3 = ADC_TRIG_CTMU | ADC_REF_VDD_VDD | ADC_REF_VDD_VSS;
+	config3 = ADC_REF_VDD_VDD | ADC_REF_VDD_VSS;
 
 	OpenADC(config1,config2,config3);
 
 	ADRESH=0;//clear the ADC result register
 	ADRESL=0;//clear the ADC result register
-	
-	/* Read ADC*/
-	ConvertADC(); // stop sampling and starts adc conversion
-	while(BusyADC()); //wait untill the conversion is completed
-	adccount = ReadADC();//read the result of conversion
+
+    for (i = 0; i < 127; i++){
+		/* Read ADC*/
+		ConvertADC(); // stop sampling and starts adc conversion
+		while(BusyADC()); //wait untill the conversion is completed
+		adccount[i] = ReadADC();//read the result of conversion
+    }
 	
 	while(1);//End of program
 }
