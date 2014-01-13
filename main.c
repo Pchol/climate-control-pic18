3c2b6c8f11e0dc3f2b091aa56b221a7b9e68ce1f
Pchol <spchols@gmail.com>
2014-01-13 16:02:02 +0400

    проверка работоспособносит модуля ацп

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 
 #include "config.h"
 #include <plib/adc.h>
-#include <delays.h>
 #include <plib/ctmu.h>
 
 #define USE_OR_MASKS
@@ -11,23 +10,21 @@
 void main(void) {
 
     unsigned char config1 = 0, config2 = 0, config3 = 0;
-    unsigned int adccount[127], i=0;
+    unsigned int adccount = 0, i=0;
 
-	config1 = ADC_FOSC_RC | ADC_RIGHT_JUST | ADC_2_TAD ;
-	config2 = ADC_CH2 | ADC_INT_OFF;
-	config3 = ADC_REF_VDD_VDD | ADC_REF_VDD_VSS;
+	config1 = ADC_LEFT_JUST;
+	config2 = ADC_CH16;
+	config3 = ADC_REF_VDD_VDD & ADC_REF_VDD_VSS;
 
 	OpenADC(config1,config2,config3);
 
 	ADRESH=0;//clear the ADC result register
 	ADRESL=0;//clear the ADC result register
 
-    for (i = 0; i < 127; i++){
-		/* Read ADC*/
-		ConvertADC(); // stop sampling and starts adc conversion
-		while(BusyADC()); //wait untill the conversion is completed
-		adccount[i] = ReadADC();//read the result of conversion
-    }
+	/* Read ADC*/
+	ConvertADC(); // stop sampling and starts adc conversion
+	while(BusyADC()); //wait untill the conversion is completed
+	adccount = ReadADC();//read the result of conversion
 	
 	while(1);//End of program
 }
