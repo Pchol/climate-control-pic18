47c45e574ee706c68195caab8dc1a468d380d54e
Pchol <spchols@gmail.com>
2014-01-20 17:24:51 +0400

    добавлена функция измерения емкости

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ struct {
 //    float adc;
 //    float humidutyWather;
 //    float humidutySoil;
+    float capacitance;
     char temp[2];
 } data;
 
@@ -55,11 +56,44 @@ void measurementTemp(void){
   StopI2C1();
 }
 
-void measurement(){
+void measurementC(void){
+
+  unsigned int Vread=0;
+  float voltage;// Vcal=0, CTMUISrc = 0;
+  float time = 15;
+  float current = 5.5;//experiment time*current
+  float ownCap = 33.8;//own capacitance
+  float v = 5.0;
+
+  CTMUCONHbits.CTMUEN = 1; //Enable the CTMU
+  CTMUCONLbits.EDG1STAT = 0; // Set Edge status bits to zero
+  CTMUCONLbits.EDG2STAT = 0;
+  CTMUCONHbits.IDISSEN = 1; //drain charge on the circuit
+  __delay_us(15);
+  CTMUCONHbits.IDISSEN = 0; //end drain of circuit
+
+  CTMUCONLbits.EDG1STAT = 1;//Begin charging the circuit
+  __delay_us(15);
+  CTMUCONLbits.EDG1STAT = 0; 	 //Stop charging circuit
+
+  PIR1bits.ADIF = 0; //make sure A/D Int not set
+  ADCON0bits.GO=1; //and begin A/D conv.
+  while(!PIR1bits.ADIF); //Wait for A/D convert complete
+  PIR1bits.ADIF = 0; //Clear A/D Interrupt Flag
+  
+  Vread = ADRES;
+
+  voltage = (float)(v*Vread/1024.0);
+  data.capacitance = (float)(time*current/voltage - ownCap);//result in pf
+}
+
+
+void measurement(void){
   //измерение влажности почвы
   //измерение влажности воздуха
   //измерение температуры
   measurementTemp();
+  measurementC();
 
 }
 
@@ -91,6 +125,11 @@ void execution(void){
 	}
 }
 
+void configPorts(void){
+	TRISB = 0;
+//	PORTB = 0;
+}
+
 void configI2C(void){
 	TRISC = 0x18;//input rc3 and rc4 
 //	SSP1ADD = 0x09;					/* 100KHz (Fosc = 4MHz) */
@@ -101,16 +140,44 @@ void configI2C(void){
 	DisableIntI2C1;
 }
 
-void configPorts(void){
-	TRISB = 0;
-//	PORTB = 0;
+void configCTMU(void){
+  //CTMUCONH/1 - CTMU Control registers
+  CTMUCONH = 0x00;
+  //make sure CTMU is disabled
+  CTMUCONL = 0x90;
+  //CTMU continues to run when emulator is stopped,CTMU continues
+  //to run in idle mode,Time Generation mode disabled, Edges are blocked
+  //No edge sequence order, Analog current source not grounded, trigger
+  //output disabled, Edge2 polarity = positive level, Edge2 source =
+  //source 0, Edge1 polarity = positive level, Edge1 source = source 0,
+  //CTMUICON - CTMU Current Control Register
+  CTMUICON = 0x01;
+  //0.55uA, Nominal - No Adjustment
+  /**************************************************************************/
+  //Set up AD converter;
+  /**************************************************************************/
+  TRISA=0x04;
+  //set channel 2 as an input
+  // Configure AN2 as an analog channel
+  ANSELAbits.ANSA2=1;
+  TRISAbits.TRISA2=1;
+  // ADCON2
+  ADCON2bits.ADFM=1;
+  ADCON2bits.ACQT=1;
+  ADCON2bits.ADCS=2;
+  // ADCON1
+  ADCON1bits.PVCFG0 =0;
+  ADCON1bits.NVCFG1 =0;
+  // ADCON0
+  ADCON0bits.CHS=2;
+  ADCON0bits.ADON=1;
 }
-
 
 //function initializiation all components
 void init(){
     configPorts();
     configI2C();
+    configCTMU();
 //    configADC();
 //    configCTMU();
 
