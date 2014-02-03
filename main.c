72f5ac11a3f9c92bff7842c8a3e04af87a16c40c
Pchol <spchols@gmail.com>
2014-02-03 18:19:41 +0400

    конфигурирование модуля ацп

--- main.c
+++ main.c
@@ -9,22 +9,27 @@
 
 void main(void) {
 
-    unsigned char config1 = 0, config2 = 0, config3 = 0;
-    unsigned int adccount = 0, i=0;
+//    unsigned char config1 = 0, config2 = 0, config3 = 0;
+    int adccount = 0, i=0;
 
-	config1 = ADC_LEFT_JUST;
-	config2 = ADC_CH16;
-	config3 = ADC_REF_VDD_VDD & ADC_REF_VDD_VSS;
+//	config1 = ADC_LEFT_JUST;
+//	config2 = ADC_CH16;
+//	config3 = ADC_REF_VDD_VDD & ADC_REF_VDD_VSS;
 
-	OpenADC(config1,config2,config3);
+//	OpenADC(config1,config2,config3);
+    ADCON0 = 0b00000001;
+    ADCON1 = 0b00001111;
+    ADCON2 = 0b10001000;
+    
 
-	ADRESH=0;//clear the ADC result register
-	ADRESL=0;//clear the ADC result register
+    while(1){
+		ADRESH=0;//clear the ADC result register
+		ADRESL=0;//clear the ADC result register
 
-	/* Read ADC*/
-	ConvertADC(); // stop sampling and starts adc conversion
-	while(BusyADC()); //wait untill the conversion is completed
-	adccount = ReadADC();//read the result of conversion
+		/* Read ADC*/
+		ConvertADC(); // stop sampling and starts adc conversion
+		while(BusyADC()); //wait untill the conversion is completed
+		adccount = ReadADC();//read the result of conversion
+    }
 	
-	while(1);//End of program
 }
