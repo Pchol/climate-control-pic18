9d0bacae7023af221374f9be258983e2d94ca648
Pchol <spchols@gmail.com>
2014-02-06 16:46:28 +0400

    перевод в напряжение

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 void main(void) {
 
 //    unsigned char config1 = 0, config2 = 0, config3 = 0;
-    int adccount = 0, i=0;
+    float adccount = 0;
 
 //	config1 = ADC_LEFT_JUST;
 //	config2 = ADC_CH16;
@@ -23,13 +23,14 @@ void main(void) {
     
 
     while(1){
+
 		ADRESH=0;//clear the ADC result register
 		ADRESL=0;//clear the ADC result register
 
 		/* Read ADC*/
 		ConvertADC(); // stop sampling and starts adc conversion
 		while(BusyADC()); //wait untill the conversion is completed
-		adccount = ReadADC();//read the result of conversion
+		adccount = 3.3*(float)ReadADC()/1023;//read the result of conversion
     }
 	
 }
